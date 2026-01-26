e392d76095305e31454f6451df92581c33257089
Geoff Little <g.little712@gmail.com>
2026-01-26 15:28:39 -0500

    feat: add vanilla components

---
+++ packages/components/src/components/HealthCheck.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useApi } from '../hooks/useApi';
+import { useApiClient } from '../contexts/ApiContext';
+
+interface HealthCheckRenderProps {
+  error: string | null;
+  status: 'ok' | 'fail' | undefined;
+  uptime: number | undefined;
+}
+
+interface HealthCheckProps {
+  children: (props: HealthCheckRenderProps) => React.ReactNode;
+}
+
+const HealthCheck: React.FC<HealthCheckProps> = ({ children }) => {
+  const api = useApiClient();
+  const { data: health, error } = useApi(() => api.healthGet(), [], { pollInterval: 2000 });
+
+  return <>{children({
+    error,
+    status: health?.status,
+    uptime: health?.uptime,
+  })}</>;
+};
+
+export default HealthCheck;
