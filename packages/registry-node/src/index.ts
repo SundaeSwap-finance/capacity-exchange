7cf67b86cb8876a82dd1f4f89329b2b7dc89c27f
Simon Gellis <simongellis@gmail.com>
2026-06-25 17:00:00 -0400

    fix: correct registry-node compilation

--- packages/registry-node/src/index.ts
+++ packages/registry-node/src/index.ts
@@ -1,3 +1,4 @@
 export { register, type RegisterParams } from './circuits/register.js';
 export { deregister, type DeregisterParams } from './circuits/deregister.js';
 export { deploy, type DeployOutput } from './deploy.js';
+export { generateRandomSecretKey } from './types.js';
